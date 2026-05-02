2a5227b6b15413d2598d61bca06551e3f0d5533c
MrGoddar <134316279+MrGoddar@users.noreply.github.com>
2026-05-02 01:41:57 -0300

    Finalizando exercicios de SQL e NoSQL conforme feedback

--- exercicio_sql.sql
+++ exercicio_sql.sql
@@ -14,3 +14,16 @@ FROM actor a
 JOIN film_actor fa ON a.actor_id = fa.actor_id
 JOIN film f ON fa.film_id = f.film_id
 WHERE f.length > 120;
+
+SELECT title, rating, length 
+FROM film 
+WHERE rating = 'PG-13' AND length > 100;
+
+SELECT rating, COUNT(*) as total 
+FROM film 
+GROUP BY rating;
+
+SELECT c.first_name, c.last_name, r.rental_date
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+LIMIT 10;
--- filtros_nosql.js
+++ filtros_nosql.js
@@ -1,8 +1,22 @@
-// 1. Livros com status diferente de "PUBLISH" e menos de 300 páginas
 { "status": { "$ne": "PUBLISH" }, "pageCount": { "$lt": 300 } }
 
-// 2. Livros com "MongoDB" ou "SQL Server" no título
 { "title": { "$in": [/MongoDB/i, /SQL Server/i] } }
 
-// 3. Livros com ISBN começando com "18"
 { "isbn": /^18/ }
+
+db.books.insertOne({
+    "title": "Manual do QA Matheus",
+    "isbn": "123456",
+    "status": "PUBLISH",
+    "authors": ["Matheus EBAC"],
+    "categories": ["Software Testing"]
+});
+
+db.books.find({ categories: "Java" });
+
+db.books.updateOne(
+    { title: "Manual do QA Matheus" },
+    { $set: { status: "MEAP" } }
+);
+
+db.books.countDocuments({});
